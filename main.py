349d2fcd5e70b80fbd4150d0be95e5076573f358
Arthuranjo <arthurdosanjosnascimento@gmail.com>
2025-05-05 11:50:14 -0300

    testando a conexão com o Msql

---
+++ main.py
@@ -0,0 +1,16 @@
+from database import db  # ou o caminho correto até seu db.py
+
+#def testar_conexao():
+ #   try:
+        #conexao = db.conectar()
+        #cursor = conexao.cursor()
+        #cursor.execute("SELECT 1")
+        #resultado = cursor.fetchone()
+        #print("✅ Conexão com o banco funcionando:", resultado)
+        #cursor.close()
+        #conexao.close()
+    #except Exception as e:
+        #print("❌ Erro ao conectar com o banco:", e)
+
+# Executa o teste ao iniciar
+#testar_conexao()
